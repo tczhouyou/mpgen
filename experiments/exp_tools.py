d0e30ad72b328688b08da1ec42ce5c66765b2ad6
zhou <you.zhou@kit.edu>
2020-07-22 16:13:20 +0200

    added exp_tools for choosing the equally distributed queries

---
+++ experiments/exp_tools.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def get_training_data_from_2d_grid(ndata_for_each_dim, queries):
+    minq = np.min(queries, axis=0)
+    maxq = np.max(queries, axis=0)
+
+    x = np.linspace(minq[0], maxq[0], ndata_for_each_dim)
+    y = np.linspace(minq[1], maxq[1], ndata_for_each_dim)
+
+    xv, yv = np.meshgrid(x, y)
+
+    results = []
+    ids = []
+    for i in range(np.shape(xv)[0]):
+        for j in range(np.shape(yv)[0]):
+            deq = np.array([xv[i,j], yv[i,j]])
+            ndata = np.shape(queries)[0]
+            deqmat = np.tile(deq, (ndata, 1))
+            idx = np.argmin(np.linalg.norm(deqmat - queries, axis=1))
+            results.append(queries[idx,:])
+            ids.append(idx)
+            queries = np.delete(queries, idx, 0)
+
+    results = np.stack(results)
+    return results, ids
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    queries = np.loadtxt('mujoco/hitball/hitball_mpdata_v2/hitball_queries.csv', delimiter=',')
+    trqueries, ids = get_training_data_from_2d_grid(7, queries)
+    plt.plot(queries[:,0], queries[:,1], 'ro')
+    plt.plot(trqueries[:,0], trqueries[:,1], 'bo')
+    plt.show()
